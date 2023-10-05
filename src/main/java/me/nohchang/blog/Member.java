30f8c0983af169134168ee03b9f4d4b818bdd409
nohchang <nchq2123@naver.com>
2023-10-05 17:52:23 +0900

    ORM

--- src/main/java/me/nohchang/blog/Member.java
+++ src/main/java/me/nohchang/blog/Member.java
@@ -6,17 +6,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자
 @AllArgsConstructor
 @Getter
 @Entity
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // 기본키 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 자동으로 1씩 증가(데이터베이스에 위임)
     @Column(name = "id", updatable = false)
     private Long id; // DB 테이블의 'id' 컬럼과 매칭
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false) // name이라는 이름과 not null 컬럼 매칭
     private String name; // DB 테이블의 'name' 컬럼과 매칭
 }
