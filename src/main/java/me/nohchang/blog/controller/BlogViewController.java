41f048063f68ff149eb8b9212597cce5202491af
nohchang <nchq2123@naver.com>
2023-10-06 15:40:45 +0900

    블로그 화면(타임리프)

---
+++ src/main/java/me/nohchang/blog/controller/BlogViewController.java
@@ -0,0 +1,56 @@
+package me.nohchang.blog.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.nohchang.blog.domain.Article;
+import me.nohchang.blog.dto.ArticleListViewResponse;
+import me.nohchang.blog.dto.ArticleViewResponse;
+import me.nohchang.blog.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    // id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+
+        if (id == null) { // id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else { // id가 있으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
+    }
+}
---
+++ src/main/java/me/nohchang/blog/dto/ArticleListViewResponse.java
@@ -0,0 +1,18 @@
+package me.nohchang.blog.dto;
+
+import lombok.Getter;
+import me.nohchang.blog.domain.Article;
+
+@Getter
+public class ArticleListViewResponse {
+
+    private final Long id;
+    private final String title;
+    private final String content;
+
+    public ArticleListViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
